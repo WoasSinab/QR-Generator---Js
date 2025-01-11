78612017483d334cef70d9216d4e819d5ad7f9ad
Sinab <mrsaher83@gmail.com>
2025-01-11 21:15:23 +0330

    flicker added

--- script.js
+++ script.js
@@ -1,25 +1,32 @@
-function generateQRCode() {
-  const url = document.getElementById("urlInput").value;
+function generateQRCode() {  const url = document.getElementById("urlInput").value;
 
   if (!url) {
-      alert("لطفا یک آدرس وارد کنید!");
-      return;
+    alert("لطفا یک آدرس وارد کنید!");
+    return;
   }
 
   const qrcodeDiv = document.getElementById("qrcode");
   qrcodeDiv.innerHTML = "";
 
   const qrcode = new QRCode(qrcodeDiv, {
-      text: url,
-      width: 200,
-      height: 200
+    text: url,
+    width: 200,
+    height: 200,
   });
 }
 
 // حالت دارک‌مود
-const themeToggle = document.getElementById('theme-toggle');
+const themeToggle = document.getElementById("theme-toggle");
 const body = document.body;
 
-themeToggle.addEventListener('change', () => {
-  body.classList.toggle('dark-mode');
+themeToggle.addEventListener("change", () => {
+  body.classList.toggle("dark-mode");
+});
+
+const title = document.getElementById("neon-title");
+const letters = title.querySelectorAll("span");
+
+letters.forEach((letter, index) => {
+  const randomDelay = Math.random() * 2; // تاخیر تصادفی بین ۰ تا ۲ ثانیه
+  letter.style.setProperty("--delay", randomDelay);
 });
